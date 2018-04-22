ff76923e855544a1c20021d38d94331db84a0b70
Thomas Braun <braunth@oregonstate.edu>
2018-04-21 18:37:56 -0700

    Vers0

--- com/prime/Main.py
+++ com/prime/Main.py
@@ -1,6 +1,6 @@
 from com.prime.gpu.GPUMathKernel import GPUMathKernel
 
 kernel = GPUMathKernel()
-vals = kernel.add([2, 3, 4], [5, 6, 7])
+vals = kernel.add([1, 3, 4], [5, 6, 7])
 
 print("Vals:", vals[0], vals[1], vals[2])
